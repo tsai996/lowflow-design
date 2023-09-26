ac8216f4d5bd035164965e73ccdd22466713f4b5
caixiaofeng <1941338475@qq.com>
2023-09-26 10:50:17 +0800

    vite修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,86 +1,87 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 import Unocss from 'unocss/vite'
 import {
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives,
-  transformerVariantGroup,
+    presetAttributify,
+    presetIcons,
+    presetUno,
+    transformerDirectives,
+    transformerVariantGroup,
 } from 'unocss'
 
 const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${pathSrc}/`,
+    base: 'lowflow-design',
+    resolve: {
+        alias: {
+            '~/': `${pathSrc}/`,
+        },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/element/index.scss" as *;`,
-      },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "~/styles/element/index.scss" as *;`,
+            },
+        },
     },
-  },
-  plugins: [
-    vue(),
-    VueSetupExtend(),
-    Components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['ts'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/, /\.[tj]sx?$/],
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
+    plugins: [
+        vue(),
+        VueSetupExtend(),
+        Components({
+            // allow auto load markdown components under `./src/components/`
+            extensions: ['ts'],
+            // allow auto import and register components used in markdown
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/, /\.[tj]sx?$/],
+            resolvers: [
+                ElementPlusResolver({
+                    importStyle: 'sass',
+                }),
+            ],
+            dts: 'src/components.d.ts',
         }),
-      ],
-      dts: 'src/components.d.ts',
-    }),
 
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    Unocss({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons({
-          scale: 1.2,
-          warn: true,
+        // https://github.com/antfu/unocss
+        // see unocss.config.ts for config
+        Unocss({
+            presets: [
+                presetUno(),
+                presetAttributify(),
+                presetIcons({
+                    scale: 1.2,
+                    warn: true,
+                }),
+            ],
+            transformers: [
+                transformerDirectives(),
+                transformerVariantGroup(),
+            ]
         }),
-      ],
-      transformers: [
-        transformerDirectives(),
-        transformerVariantGroup(),
-      ]
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        // 打包分类
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-        // 打包后的文件夹名称生成规则-->解决部分静态服务器无法正常返回_plugin-vue_export-helper文件
-        sanitizeFileName(name) {
-          const match = /^[a-z]:/i.exec(name)
-          const driveLetter = match ? match[0] : ''
-          return (
-              driveLetter +
-              name.substring(driveLetter.length).replace(/[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g, '')
-          )
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                // 打包分类
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+                assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+                // 打包后的文件夹名称生成规则-->解决部分静态服务器无法正常返回_plugin-vue_export-helper文件
+                sanitizeFileName(name) {
+                    const match = /^[a-z]:/i.exec(name)
+                    const driveLetter = match ? match[0] : ''
+                    return (
+                        driveLetter +
+                        name.substring(driveLetter.length).replace(/[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g, '')
+                    )
+                }
+            }
         }
-      }
     }
-  }
 })
