65f1f9b694e31dadfff860815e3697560194b952
caixiaofeng <1941338475@qq.com>
2023-09-27 10:47:31 +0800

    优化hook

--- src/views/flowDesign/hooks/useNode.ts
+++ src/views/flowDesign/hooks/useNode.ts
@@ -66,6 +66,7 @@ const useNode = (node: FlowNode) => {
             users: [],
             roles: [],
             leader: 1,
+            chooser: false,
             self: false,
             nobody: 'reject'
         } as ApprovalNode
