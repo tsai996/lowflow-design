f576929a91d896595ecd723fc81eb4e6572efdf5
caixiaofeng <1941338475@qq.com>
2023-10-12 16:43:30 +0800

    条件内容

--- src/views/flowDesign/nodes/Condition/content.vue
+++ src/views/flowDesign/nodes/Condition/content.vue
@@ -11,6 +11,8 @@ const content = ref<string>('')
 watchEffect(() => {
   if ($props.node.def) {
     content.value = '不满足条件时，进入默认条件'
+  } else if ($props.conditions.conditions.length > 0) {
+    content.value = `已设置 (${$props.conditions.conditions.length})个条件`
   } else {
     content.value = '未设置条件'
   }
