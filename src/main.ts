5852129c974e3d0b7632f144dde23b878cdaad5e
caixiaofeng <1941338475@qq.com>
2023-11-25 15:43:47 +0800

    按需载入

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// import "~/styles/element/index.scss";
+
+// import ElementPlus from "element-plus";
+// import all element css, uncommented next line
+// import "element-plus/dist/index.css";
+
+// or use cdn, uncomment cdn link in `index.html`
+
 import "~/styles/index.scss";
 import "uno.css";
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// If you want to use ElMessage, import it.
+import "element-plus/theme-chalk/src/message.scss";
+import "element-plus/theme-chalk/el-input-number.css";
+
 
 const app = createApp(App);
-app.use(ElementPlus);
+// app.use(ElementPlus);
 app.mount("#app");
