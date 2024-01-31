02c7f70cbe8c4009a24768bf357a8ec1b2860be7
caixiaofeng <1941338475@qq.com>
2024-01-31 10:17:05 +0800

    修改接口

--- src/api/modules/model.ts
+++ src/api/modules/model.ts
@@ -2,7 +2,7 @@ import http from '~/api/index'
 import FileSaver from "file-saver";
 
 export const downloadXml = async (data: object) => {
-    const res = await http.download('https://www.666cxf.com/stage-api/workflow/model/download', data)
+    const res = await http.download('https://www.666cxf.com/api/workflow/model/download', data)
     FileSaver.saveAs(
         new Blob([res], { type: 'application/octet-stream;charset=utf-8' }),
         '测试流程.bpmn20.xml'
