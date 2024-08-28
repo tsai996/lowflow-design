0b7580e362005ce1da8f3d6958f3dc602b1d8a4a
caixiaofeng <1941338475@qq.com>
2024-08-28 23:38:18 +0800

    转bpmn接口修改

--- src/api/modules/model.ts
+++ src/api/modules/model.ts
@@ -2,7 +2,7 @@ import http from '@/api'
 import FileSaver from 'file-saver'
 
 export const downloadXml = async (data: object) => {
-  const res = await http.download('https://www.666cxf.com/api/model/download', data)
+  const res = await http.download('https://demo.lowflow.vip/api/model/download', data)
   FileSaver.saveAs(
     new Blob([res], { type: 'application/octet-stream;charset=utf-8' }),
     '测试流程.bpmn20.xml'
